e9617c161b9e01a8d29615e3539f475d11446b71
JuliaMuk <mukhina_ur@mail.ru>
2023-03-18 11:06:39 +0500

    connect with db

--- index.php
+++ index.php
@@ -8,7 +8,24 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Список студентов</h1>
+  <h1>Список студентов</h1>
+  <?php
+    define("HOST","localhost");
+    define("USER","root");
+    define("PASS", "root");
+    define("DB", "mydb");
+
+    $connect = new mysqli(HOST, USER, PASS, DB);
+
+    $sql = "SELECT * FROM students";
+    $result = $connect->query($sql);
+
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+      echo "<p>
+              $row[lname] $row[fname] $row[age]
+            </p>";
+    } 
+  ?>
     
 </body>
 </html>
